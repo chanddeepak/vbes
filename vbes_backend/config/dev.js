e3e2ce1a8669bd8182b9efc3cd7e64d84a78db4b
Deepak Chand <deepu996@gmail.com>
2019-03-17 23:00:51 +0530

    Completed email sending and save to firebase database

---
+++ vbes_backend/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  sendGridKey:
+    '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'
+};
---
+++ vbes_backend/routes/email.js
@@ -0,0 +1,56 @@
+// const express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const firebase = require('../initFirebase');
+
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const db = firebase.database();
+const ref = db.ref('email');
+
+router.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+router.post('/send', async (req, res) => {
+  //const { title, subject, body, recipient, sender } = req.body;
+
+  //console.log(recipient);
+
+  const survey = {
+    title: 'Test',
+    subject: 'Test',
+    body: 'Test Mail',
+    recipient: 'deepu996@gmail.com',
+    sender: 'chanddeepakop07@gmail.com',
+    dateSent: Date.now()
+  };
+
+  const mailer = new Mailer(survey, surveyTemplate(survey));
+
+  try {
+    await mailer.send();
+    // await survey.save();
+    // const user = await req.user.save();
+    const sentEmail = ref.child('sent');
+    sentEmail.set(survey);
+
+    res.send({
+      response: 'Success'
+    });
+  } catch (err) {
+    res.status(422).send(err);
+  }
+});
+
+module.exports = router;
